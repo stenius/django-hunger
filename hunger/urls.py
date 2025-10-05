df395b1a8ffc46f03eca323e56cfeb91c4470bcd
Paul Stenius <stenius@users.noreply.github.com>
2025-10-05 17:48:05 -0400

    Update urls.py

--- hunger/urls.py
+++ hunger/urls.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
-from django.conf.urls import url
+from django.urls import re_path as url
 from .views import (InviteView, VerifiedView, InvalidView, NotBetaView,
                     InviteSentView, verify_invite)
 
